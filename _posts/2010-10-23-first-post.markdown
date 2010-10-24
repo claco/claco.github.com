1a84782529e7d7ae509005e97da0c3bddeb6c14c
Christopher H. Laco <claco@chrislaco.com>
2010-10-24 18:28:22 -0400

    Convert and import WP/MT entries back to 2002

--- _posts/2010-10-23-first-post.markdown
+++
@@ -1,6 +0,0 @@
----
-layout: post
-title: Welcome to 'Claco.github.com'
----
-
-Claco.github.com is going to be an awesome project.
---
+++ _scripts/convert.pl
@@ -0,0 +1,122 @@
+#!/usr/bin/env perl -wT
+use strict;
+use warnings;
+use FileHandle;
+use DateTime;
+use DateTime::Format::Strptime;
+
+no strict 'refs';
+
+my $file = FileHandle->new('< export.txt');
+
+if (defined $file) {
+  my $defaults = {
+    category => 'OffTopic', 
+    tags => []
+  };
+  my $post = {%{$defaults}};
+
+  while (my $line = $file->getline()) {
+    if ($line =~ /^TITLE: (.*)$/) {
+      $post->{title} = $1;
+    }; 
+
+    if ($line =~ /^BASENAME: (.*)$/) {
+      my $slug = lc $1;
+      $slug =~ s/_/-/g;
+      $post->{slug} = $slug; 
+    };
+
+    if ($line =~ /^PRIMARY CATEGORY: (.*)$/) {
+      my $category = $1;
+      $category =~ s/\?//g;
+
+      $post->{category} = $category;
+    };
+
+    if ($line =~ /^TAGS: (.*)$/) {
+      my $tags = lc $1;
+      my @tags = split(/,\s*/, $tags);
+
+      for (@tags) {
+        s/\"//g; 
+      };
+
+      $post->{tags} = \@tags;
+    };
+
+    if ($line =~/^COMMENT:$/) {
+      $post->{incomments} = 1;    
+    };
+
+    if ($line =~ /^DATE: (.*)$/) {
+      if (! $post->{incomments}) {
+      
+        my $format = DateTime::Format::Strptime->new(
+          pattern => '%m/%d/%Y %l:%M:%S %p',
+          time_zone => "America/New_York"
+        );
+        my $date = $format->parse_datetime($1);
+        my $day = $date->ymd;
+        my $slug = $post->{slug};
+        my $filename = "$day-$slug.textile";
+
+        $post->{date} = $date;    
+        $post->{filename} = $filename;
+      }
+    };
+
+    if ($line =~ /^BODY:$/) {
+      my $bline = $file->getline;
+
+      until ($bline =~ /^-----$/) {
+        $post->{body} .= $bline;
+
+        $bline = $file->getline; 
+      };
+    };
+
+    if ($line =~ /^EXTENDED BODY:$/) {
+      my $bline = $file->getline;
+
+      until ($bline =~ /^-----$/) {
+        $post->{extended} .= $bline;
+
+        $bline = $file->getline; 
+      };
+    };
+
+    if ($line =~ /^--------$/) {
+      writePost($post);
+      $post = {};
+      $post = {%{$defaults}};
+    }
+  };
+  $file->close;
+};
+
+sub writePost() {
+  my $post = shift;
+  my $filename = '../_posts/' . $post->{filename};
+  my $file = FileHandle->new("> $filename");
+  my @tags = @{$post->{tags}};
+  my $title = $post->{title};
+  $title =~ s/"/\\"/g;
+
+  $file->print("---", "\n");
+  $file->print("layout: post", "\n");
+  $file->print("title: \"", $title, "\"\n");
+  $file->print("category: ", $post->{category}, "\n");
+  if (scalar @tags) {
+    $file->print("tags:\n  - ", join('  - ', map{$_ . "\n"} @tags));
+  };
+  $file->print("published:\n");
+  $file->print("  epoch: ", $post->{date}->epoch, "\n");
+  $file->print("  utc: ", $post->{date}->set_time_zone('UTC')->datetime, "\n");
+  $file->print("---", "\n\n");
+  $file->print($post->{body});
+  $file->print($post->{extended}, "\n");
+
+  $file->close;
+  utime $post->{date}->epoch, $post->{date}->epoch, $filename;
+};
